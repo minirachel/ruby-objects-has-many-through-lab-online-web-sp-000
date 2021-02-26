cb7a599ceb636bbc7534699d36a93f9cfc0c9515
minirachel <rachel.a.levinson@gmail.com>
2021-02-26 16:46:35 -0500

    commit tree 8e9a063626324c4b782cab00f7338691c22e57c9

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|s| s.genre == self}
+    end
+
+    def artists
+        songs.map {|a| a.artist}
+    end
+
+
+end
